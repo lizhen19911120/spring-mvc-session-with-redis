135083e5560c6e64167bc28c831601ce946fd940
lizhen <1553164564@qq.com>
2018-11-19 15:52:26 +0800

    initialize

---
+++ src/main/java/cn/kiiwii/framework/filter/MyFilter.java
@@ -0,0 +1,24 @@
+package cn.kiiwii.framework.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by lizhen on 2018/11/19.
+ */
+public class MyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
